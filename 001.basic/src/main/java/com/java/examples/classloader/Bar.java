9f75766542293d4fa64dcbc6785d5d24351da028
姜睿 <rui.jiang@pekall.com>
2020-02-01 22:35:33 +0800

    修改应用分页排序重复列表项问题

--- 001.basic/src/main/java/com/java/examples/classloader/Bar.java
+++ 001.basic/src/main/java/com/java/examples/classloader/Bar.java
@@ -1,10 +1,13 @@
 // package com.java.examples.classloader;
 
-public class
-Bar {
+public class Bar {
 
     int msg(String msg) {
         System.out.println("bar: " + msg);
         return 0;
+    }
+
+    public static void main(String[] args) {
+        new Bar().msg("wahaha!");
     }
 }
--- 001.basic/src/main/java/com/java/examples/classloader/build.sh
+++ 001.basic/src/main/java/com/java/examples/classloader/build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
 javac Foo.java Bar.java
-# jar -cvf foo.jar Foo.class Bar.class
-jar -cvfm foo.jar META-INF/MANIFEST.MF Foo.class Bar.class
 
-java -jar foo.jar
+buildWithMain() {
+  jar -cvfm foo.jar META-INF/MANIFEST.MF Foo.class Bar.class
+  java -jar foo.jar
+}
+
+buildWithoutMain() {
+  jar -cvf foo.jar Foo.class Bar.class
+  java -classpath foo.jar Foo
+  java -classpath foo.jar Bar
+}
+
+buildWithoutMain
