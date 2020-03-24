0fca5f8e7c590f411acd9b8c8b2693f2e5cd84b5
姜睿 <rui.jiang@pekall.com>
2020-03-24 08:04:14 +0800

    update

---
+++ 001.basic/src/main/java/com/leetcode/tree/L429NthTreeLevelTrav.java
@@ -0,0 +1,21 @@
+package com.leetcode.tree;
+
+import java.util.*;
+
+/**
+ * 429. N叉树的层序遍历
+ * 给定一个 N 叉树，返回其节点值的层序遍历。 (即从左到右，逐层遍历)。
+ *
+ */
+public class L429NthTreeLevelTrav {
+
+    public List<List<Integer>> levelOrder(Node root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+
+        Queue<Node> queue = new LinkedList<>();
+
+    }
+}
