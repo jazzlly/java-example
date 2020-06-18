2471e7e9e7b730840828a58549bd9b98c7f4b4fe
ryan <jiangrui.tr2008@gmail.com>
2020-06-18 09:36:29 +0800

    updated

--- 001.basic/src/test/java/com/leetcode/array/L283MoveZerosTest.java
+++ 001.basic/src/test/java/com/leetcode/array/L283MoveZerosTest.java
@@ -7,11 +7,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class L283MoveZerosTest {
 
-    @Test
-    public void smoke() {
-        int[] array = new int[]{0, 1, 0, 3, 12};
-        L283MoveZeros.moveZeroes(array);
-
-        assertThat(array).containsExactly(1, 3, 12, 0, 0);
-    }
+//    @Test
+//    public void smoke() {
+//        int[] array = new int[]{0, 1, 0, 3, 12};
+//        L283MoveZeros.moveZeroes(array);
+//
+//        assertThat(array).containsExactly(1, 3, 12, 0, 0);
+//    }
 }
