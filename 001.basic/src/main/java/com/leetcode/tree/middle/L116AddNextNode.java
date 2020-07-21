5b1675242e9fc0a9b70c6d052181b658d478b846
ryan <jiangrui.tr2008@gmail.com>
2020-07-21 09:54:45 +0800

    update

--- 001.basic/src/main/java/com/leetcode/tree/middle/L116AddNextNode.java
+++ 001.basic/src/main/java/com/leetcode/tree/middle/L116AddNextNode.java
@@ -1,5 +1,7 @@
 package com.leetcode.tree.middle;
 
+import com.leetcode.tree.utils.TreeUtils;
+
 /**
  * 116. 填充每个节点的下一个右侧节点指针
  * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
@@ -15,5 +17,38 @@ package com.leetcode.tree.middle;
  * 初始状态下，所有 next 指针都被设置为 NULL。
  */
 public class L116AddNextNode {
+    public Node connect(Node root) {
+        recursion(root);
+        return root;
+    }
+
+    private void recursion(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        Node left = node.left;
+        Node right = node.right;
+        while (left != null && right != null) {
+            left.next = right;
+            left = left.right;
+            right = right.left;
+        }
+
+        recursion(node.left);
+        recursion(node.right);
+    }
+
+    public static void main(String[] args) {
+
+        Node root = TreeUtils.makeNodeBinaryTree(
+                1, 2, 3, 4, 5, 6, 7);
+
+        L116AddNextNode test = new L116AddNextNode();
+        Node ans = test.connect(root);
+        System.out.println("haha!");
+    }
 
 }
+
+
--- 001.basic/src/main/java/com/leetcode/tree/utils/TreeUtils.java
+++ 001.basic/src/main/java/com/leetcode/tree/utils/TreeUtils.java
@@ -1,6 +1,7 @@
 package com.leetcode.tree.utils;
 
 import com.leetcode.tree.TreeNode;
+import com.leetcode.tree.middle.Node;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -27,6 +28,42 @@ public class TreeUtils {
 
         for (int i = 0; i < treeNodes.size(); i++) {
             TreeNode current = treeNodes.get(i);
+            if (current == null) {
+                continue;
+            }
+
+            int leftIndex = 2 * i + 1;
+            int rightIndex = 2 * i + 2;
+            if (leftIndex < treeNodes.size()) {
+                current.left = treeNodes.get(leftIndex);
+            }
+            if (rightIndex < treeNodes.size()) {
+                current.right = treeNodes.get(rightIndex);
+            }
+        }
+        return treeNodes.get(0);
+    }
+
+    public static Node makeNodeBinaryTree(Integer ... a) {
+        return makeNodeBinaryTree(Arrays.asList(a));
+    }
+
+    public static Node makeNodeBinaryTree(List<Integer> list) {
+        if (list == null || list.size() == 0) {
+            return null;
+        }
+
+        List<Node> treeNodes = new ArrayList<>();
+        for (Integer integer : list) {
+            if (integer != null) {
+                treeNodes.add(new Node(integer));
+            } else {
+                treeNodes.add(null);
+            }
+        }
+
+        for (int i = 0; i < treeNodes.size(); i++) {
+            Node current = treeNodes.get(i);
             if (current == null) {
                 continue;
             }
