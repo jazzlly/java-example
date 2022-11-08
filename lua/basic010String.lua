95d740700d921ef36dc6754d7df7e11817c7037d
Ryan Jiang <rui.jiang@pekall.com>
2022-11-08 18:49:13 +0800

    修改登录日志无法上报

--- lua/basic010String.lua
+++ lua/basic010String.lua
@@ -22,7 +22,7 @@ local s2 = "123456789"
 print(string.format("0x%x, %d, %c", s2:byte(1), s2:byte(1), s2:byte(1)))
 
 
--- 将数据类型打包为byte stream
+-- 将数据(int, long, double, ...)打包为byte stream
 -- <是小端， =是本地cpu字节序; 格式代码参见 https://wiki.luatos.com/_static/lua53doc/manual.html#6.4.2
 local s3 = string.pack('=i', 0x12345678)
 print(s3:byte(1, -1))
@@ -43,6 +43,34 @@ print(type(v1), type(p1)) -- number, number
 print(string.unpack("=i", s5, 5))
 -- print(string.unpack("=i", s5, 9))
 
-
-
 print(string.rep("haha", 5, ','))
+
+
+-- 字节序
+-- string.pack 将各种类型的number, 包括int, uint, short等等转化为string(byte[])
+-- c: uint32_t n = 0x12345678
+local data = string.pack('<L', 0x12345678)
+local data2 = string.char(0x78, 0x56, 0x34, 0x12)
+print(#data, #data2)
+print(type(data), type(data2))   -- string, lua使用string表示byte[]
+print(data:byte(1, -1))
+print(data2:byte(1, -1))
+
+-- string.unpack将string(byte[])转化为number
+local aunit32 = string.unpack("<L", data)
+print(type(aunit32), aunit32, 0x12345678) 
+
+local u1, u2 = string.unpack("<LL", data..data)
+print(u1, u2)
+
+
+--[[
+将数据填充到C结构体
+typedef struct test {
+    unsigned char c1,
+    unsigned char c2,
+    unsigned short s1
+}
+]]
+local c1, c2, s1 = string.unpack('<BBH', data)
+print(c1, c2, s1) -- 0x78, 0x56, 0x1234
