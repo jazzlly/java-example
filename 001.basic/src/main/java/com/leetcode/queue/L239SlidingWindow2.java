355564e1631c0b6fb2790a5caaaa260486ce6871
姜睿 <rui.jiang@pekall.com>
2023-02-19 21:31:01 +0800

    update

---
+++ 001.basic/src/main/java/com/leetcode/queue/L239SlidingWindow2.java
@@ -0,0 +1,23 @@
+package com.leetcode.queue;
+
+import java.util.*;
+
+/**
+ * 239. 滑动窗口最大值
+ * 给定一个数组 nums，有一个大小为 k 的滑动窗口从数组的最左侧移动到数组的最右侧。
+ *  你只可以看到在滑动窗口内的 k 个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回滑动窗口中的最大值。
+ *
+ * 示例:
+ *
+ * 输入: nums = [1,3,-1,-3,5,3,6,7], 和 k = 3
+ * 输出: [3,3,5,5,6,7]
+ */
+public class L239SlidingWindow2 {
+//    public static int[] maxSlidingWindow(int[] nums, int k) {
+//
+//
+//    }
+
+}
